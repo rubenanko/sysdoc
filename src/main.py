dba944cf554fab035a08b568db6ec69ec88db8e4
rubenanko <rubenanko@gmail.com>
2025-12-11 14:35:25 +0100

    fix: absolute data file access

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from typing import List
 import argparse
 import json
-import sys
+import sys,os
 import ui
 
-with open("data.json","r") as f:
+with open(os.path.join(os.path.dirname((os.path.dirname(os.path.realpath(__file__)))),"data.json"),"r") as f:
     raw_json = f.read()
     SYSCALLS = json.loads(raw_json)
 
@@ -19,8 +19,7 @@ def main(argv : List[str])->None:
     parser.add_argument("syscall_proxy",help="display the syscall's documentation",type=str,nargs='?',default=None)
     parser.add_argument("-a","--arch",help="filters the syscalls by arch",type=str)
     parser.add_argument("-n","--name",help="filters the syscalls by name",type=str)
-    parser.add_argument("-d","--decimal",help="filters the syscalls by code, in a decimal format",action="store_true")
-    parser.add_argument("-x","--hex",help="filters the syscalls by code, in a hexadecimal format",action="store_true")
+    parser.add_argument("-c","--code",help="filters the syscalls by code",type=str)
 
     # parse argv
     args = parser.parse_args(argv)
@@ -29,11 +28,22 @@ def main(argv : List[str])->None:
         if args.syscall_proxy.isalpha():
             for syscall in SYSCALLS:
                 if syscall["name"] == args.syscall_proxy:
-                    print(syscall)
+                    print(ui.format_syscall(syscall))
         else:
-            
+            base = 16 if args.syscall_proxy[:2] == "0x" else 10
+            syscall_proxy_int = int(args.syscall_proxy,base)
+            for syscall in SYSCALLS:
+                if syscall["nr"] == syscall_proxy_int:
+                    print(ui.format_syscall(syscall))
     else:
-
+        buffer = ""
+        for syscall in SYSCALLS:
+            filters = (not args.arch) or args.arch == syscall["arch"]
+            filters = filters and ((not args.name) or args.name in syscall["name"])
+            filters = filters and ((not args.code) or int(args.code) == syscall["nr"])
+            if filters:
+                buffer += ui.format_syscall(syscall) + "\n" + "-"*30 + "\n"
+        print(buffer)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- src/ui.py
+++ src/ui.py
@@ -1,3 +1,23 @@
 
+REGISTERS = {
+    "x64" : {
+                "code": "RAX",
+                "arg0": "RDI",
+                "arg1": "RSI",
+                "arg2": "RDX",
+                "arg3": "R10",
+                "arg4": "R8",
+                "arg5": "R9",
+            }
+}
+
 def format_syscall(syscall):
-    pass
+    header = f'arch\tNR\tname\t{REGISTERS[syscall["arch"]]["code"]}\n'
+
+    args = ""
+    i = 0
+    while i < 6 and syscall[f"arg{i}"] != "":
+        args += "\n" + REGISTERS[syscall["arch"]][f"arg{i}"] + "\t" + syscall[f"arg{i}"]
+        i += 1
+
+    return f'{header}{syscall["arch"]}\t{syscall["nr"]}\t{syscall["name"]}\t{hex(syscall["nr"])}\n{args}'
